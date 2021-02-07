303aa7853a52689bb32ff1bce8dcde0e47c4d1d6
Stephen D. Gibson <stephen.d.gibson@gmail.com>
2021-02-07 13:33:42 -0500

    changed access token

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 git config --global user.name "dungeontiger"
 git config --global user.email "stephen.d.gibson@gmail.com"
-git remote set-url --push origin https://dungeontiger:32e33b3ae57397172518d3bac967d21b2d799137@github.com/dungeontiger/lexicon.git
+git remote set-url --push origin https://dungeontiger:260ed35b886d96e788df35e5d74093aebd8eeab1@github.com/dungeontiger/lexicon.git
 git checkout -b updates
 git add .
 git commit -m "lexicon updates"
