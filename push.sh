9d7a62b2d47a1680c7d9d2d01f38577e7e1bdfcb
Stephen D. Gibson <stephen.d.gibson@gmail.com>
2021-02-07 13:46:45 -0500

    removed access token

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 git config --global user.name "dungeontiger"
 git config --global user.email "stephen.d.gibson@gmail.com"
-git remote set-url --push origin https://dungeontiger:8bc7a7091df31d0b605ca0ac6ed841e396c78fa2@github.com/dungeontiger/lexicon.git
+git remote set-url --push origin https://dungeontiger:$ACCESS_TOKEN@github.com/dungeontiger/lexicon.git
 git checkout -b updates
 git add .
 git commit -m "lexicon updates"
