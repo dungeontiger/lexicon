6fe883249a99866288339e0ee17f3461c1cea275
Stephen D. Gibson <stephen.d.gibson@gmail.com>
2021-04-10 17:19:05 -0400

    moved from JSON to database

--- lexicon.py
+++ lexicon.py
@@ -1,9 +1,7 @@
 from flask import Flask, jsonify, request
-from os import listdir
-from os.path import isfile, join
 import os
 import json
-from collections import OrderedDict
+import psycopg2
 
 
 app = Flask(__name__)
@@ -17,82 +15,140 @@ def index():
 
 @app.route('/books')
 def books():
-    books = [f[:-5] for f in listdir('books') if isfile(join('books', f))]
-    books.sort()
+    books = []
+    conn = psycopg2.connect(os.environ['DATABASE_URL'])
+    cur = conn.cursor()
+    cur.execute("SELECT book_name from book ORDER BY book_name")
+    book = cur.fetchone()
+    while book is not None:
+        books.append(book[0])
+        book = cur.fetchone()
+    cur.close()
+    conn.close()
     return jsonify(books)
 
 
 @app.route('/books/newBook', methods=['POST'])
 def newBook():
     # check to see if a book with this name exists already
-    if isfile(join('books', '{}.json'.format(request.form['name']))):
-        return 'A book with the name {} already exists. No new book was created.'.format(request.form['name'])
-    # create the book file
-    with open(join('books', '{}.json'.format(request.form['name'])), 'w') as f:
-        f.write("{}")
-    return ''
+    result = ''
+    conn = psycopg2.connect(os.environ['DATABASE_URL'])
+    cur = conn.cursor()
+    cur.execute("SELECT id FROM book WHERE book_name='{}'".format(request.form['name']))
+    row = cur.fetchone()
+    if row is not None:
+        # send back an error
+        result = 'A book with the name {} already exists. No new book was created.'.format(request.form['name'])
+    else:
+        # create the book entry
+        cur.execute("INSERT INTO book (book_name) VALUES ('{}')".format(request.form['name']))
+        conn.commit()
+    cur.close()
+    conn.close()
+    return result
 
 
 @app.route('/books/delete/<book>', methods=['DELETE'])
 def deleteBook(book):
-    filename = join('books', '{}.json'.format(book))
-    os.remove(filename)
+    # delete from both tables
+    conn = psycopg2.connect(os.environ['DATABASE_URL'])
+    cur = conn.cursor()
+    # first get the id of the book
+    cur.execute("SELECT id FROM book WHERE book_name='{}'".format(book))
+    book_id = cur.fetchone()[0]
+    cur.execute("DELETE FROM book WHERE id = {}".format(book_id))
+    cur.execute("DELETE FROM definition WHERE book_id = {}".format(book_id))
+    conn.commit()
+    cur.close()
+    conn.close()
     return ''
 
 
 @app.route('/books/rename/<book>', methods=['POST'])
 def renameBook(book):
     # check to see if the new name exists, if it does fail
-    if isfile(join('books', '{}.json'.format(request.form['name']))):
-        return 'A book with the name {} already exists. No book has been renamed.'.format(request.form['name'])
-    os.rename(join('books', '{}.json'.format(book)), join('books', '{}.json'.format(request.form['name'])))
-    return ''
+    result = ''
+    conn = psycopg2.connect(os.environ['DATABASE_URL'])
+    cur = conn.cursor()
+    cur.execute("SELECT id FROM book WHERE book_name='{}'".format(request.form['name']))
+    row = cur.fetchone()
+    if row is not None:
+        # send back an error
+        result = 'A book with the name {} already exists. No new book was created.'.format(request.form['name'])
+    else:
+        # create the book entry
+        cur.execute("UPDATE book SET book_name = '{}' WHERE book_name = '{}'".format(request.form['name'], book))
+        conn.commit()
+    cur.close()
+    conn.close()
+    return result
 
 
 @app.route('/books/<book>')
 def getBook(book):
-    with open(join('books', '{}.json'.format(book)), 'r') as f:
-        defs = json.load(f)
-        ordered = {}
-        for k in sorted(defs.keys()):
-            ordered[k] = defs[k]
+    conn = psycopg2.connect(os.environ['DATABASE_URL'])
+    cur = conn.cursor()
+    # first get the id of the book
+    cur.execute("SELECT id FROM book WHERE book_name='{}'".format(book))
+    book_id = cur.fetchone()[0]
+    cur.execute("SELECT term, definition FROM definition WHERE book_id='{}' ORDER BY term".format(book_id))
+    row = cur.fetchone()
+    ordered = {}
+    while row is not None:
+        ordered[row[0]] = row[1]
+        row = cur.fetchone()
+    cur.close()
+    conn.close()
     return jsonify(ordered)
 
 
 @app.route('/books/addDef/<book>', methods=['POST'])
 def addDef(book):
-    # read in the book
-    with open(join('books', '{}.json'.format(book)), 'r') as f:
-        defs = json.load(f)
+    conn = psycopg2.connect(os.environ['DATABASE_URL'])
+    cur = conn.cursor()
+    # first get the id of the book
+    cur.execute("SELECT id FROM book WHERE book_name='{}'".format(book))
+    book_id = cur.fetchone()[0]
+    result = ''
     word = request.form['word']
     notes = request.form['notes']
-    # add the definition (if it does not exist)
-    if word in defs:
-        return "A definition for the word " + word + " already exists and was not replaced or added."
-    # write out the book
-    defs[word] = notes
-    with open(join('books', '{}.json'.format(book)), 'w') as f:
-        json.dump(defs, f)
-    return ''
+    # check and see if a definition for this already exists
+    cur.execute("SELECT id FROM definition where book_id={} and term = '{}'".format(book_id, word))
+    row = cur.fetchone()
+    if row is not None:
+        result = "A definition for the word " + word + " already exists and was not replaced or added."
+    else:
+        cur.execute("INSERT INTO definition (book_id, term, definition) VALUES({}, '{}', '{}')".format(book_id, word, notes))
+        conn.commit()
+    cur.close()
+    conn.close()
+    return result
 
 
 @app.route('/books/deleteDef/<book>', methods=['DELETE'])
 def deleteDef(book):
-    # read in the book
-    with open(join('books', '{}.json'.format(book)), 'r') as f:
-        defs = json.load(f)
-    del defs[request.form['word']]
-    with open(join('books', '{}.json'.format(book)), 'w') as f:
-        json.dump(defs, f)
+    conn = psycopg2.connect(os.environ['DATABASE_URL'])
+    cur = conn.cursor()
+    # first get the id of the book
+    cur.execute("SELECT id FROM book WHERE book_name='{}'".format(book))
+    book_id = cur.fetchone()[0]
+    # delete the entry
+    cur.execute("DELETE from definition WHERE book_id = {} and term = '{}'".format(book_id, request.form['word']))
+    conn.commit()
+    cur.close()
+    conn.close()
     return ''
 
 
 @app.route('/books/editDef/<book>', methods=['POST'])
 def editDef(book):
-    # read in the book
-    with open(join('books', '{}.json'.format(book)), 'r') as f:
-        defs = json.load(f)
-    defs[request.form['word']] = request.form['notes']
-    with open(join('books', '{}.json'.format(book)), 'w') as f:
-        json.dump(defs, f)
+    conn = psycopg2.connect(os.environ['DATABASE_URL'])
+    cur = conn.cursor()
+    # first get the id of the book
+    cur.execute("SELECT id FROM book WHERE book_name='{}'".format(book))
+    book_id = cur.fetchone()[0]
+    cur.execute("UPDATE definition SET definition = '{}' WHERE book_id = {} and term = '{}'".format(request.form['notes'], book_id, request.form['word']))
+    conn.commit()
+    cur.close()
+    conn.close()
     return ''
